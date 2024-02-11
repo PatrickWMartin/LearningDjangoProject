c5eca51f5e5454920a8a7b5b7f89a23cb1cb941c
Patrick Martin <patrickwaltermartin@gmail.com>
2024-02-11 15:23:47 -0400

    Add topics display page
    Added the functionality to display the topics content on a page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from  .models import Topic
 
 # Create your views here.
 def index(request):
     return render(request, 'learning_logs/index.html')
+
+
+def topics(requst):
+    topics = Topic.objects.order_by('date_added')
+    return render(requst, 'learning_logs/topics.html', {'topics': topics})
