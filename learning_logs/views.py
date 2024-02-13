4d863b0d1a3ff630adca53550b4ee09c61f75a4a
Patrick Martin <patrickwaltermartin@gmail.com>
2024-02-13 08:32:40 -0400

    Add new entry veiw logic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 # Create your views here.
@@ -26,8 +26,23 @@ def new_topic(request):
         if form.is_valid():
             form.save()
             return redirect('learning_logs:topics')
-
     else:
         form = TopicForm()
 
     return render(request, 'learning_logs/new_topic.html', {'form': form})
+
+
+def new_entry(request, topic_id):
+    topic_object = Topic.objects.get(id=topic_id)
+
+    if request.method == 'POST':
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic_object
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    else:
+        form = EntryForm()
+
+    return render(request, 'learning_logs/new_entry.html', {'form': form})
