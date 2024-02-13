e280b76099ba2d35618f637c8ac89c5f7ace8c15
Patrick Martin <patrickwaltermartin@gmail.com>
2024-02-13 08:41:29 -0400

    Register url for new entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('topics', views.all_topics, name='topics'),
     path('topics/<int:topic_id>', views.topic, name='topic'),
     path('newtopic', views.new_topic, name='new_topic'),
+    path('newentry/<int:topic_id>', views.new_entry, name='new_entry'),
 ]
